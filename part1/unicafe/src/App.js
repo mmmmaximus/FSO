897d291b9f5526c49e3115d2dfa5e8c28d0211a8
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-04-17 17:32:04 +0800

    part1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,29 @@
 import React, { useState } from 'react'
+import Header from './Header'
+import Button from './Button'
+import Stats from './Stats'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = (good / all || 0) * 100 + '%'
 
   return (
     <div>
-      code here
+      <Header header='give feedback'/>
+      <Button text='good' onClick={() => setGood(good +1)}/>
+      <Button text='neutral' onClick={() => setNeutral(neutral +1)}/>
+      <Button text='bad' onClick={() => setBad(bad +1)}/>
+      <Header header='statistics'/>
+      <Stats text='good' number={good}/>
+      <Stats text='neutral' number={neutral}/>
+      <Stats text='bad' number={bad}/>
+      <Stats text='all' number={all}/>
+      <Stats text='average' number={average}/>
+      <Stats text='positive' number={positive}/>
     </div>
   )
 }
