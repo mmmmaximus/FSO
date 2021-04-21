1f0c9536806f1f34a48cf3a8fb985af12a9bd556
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-04-21 23:37:19 +0800

    Revert "part2 (#9)"
    This reverts commit 8b3d1c25a1f95e96d21ec36309e0cfe11cc844df.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
 
 const App = () => {
   const course = {
