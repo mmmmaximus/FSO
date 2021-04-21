6a9108ef63d998598de650d4483c4fe85de4b085
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-04-21 23:36:55 +0800

    Revert "part2.1 (#10)"
    This reverts commit 58b21ca35f9cc8a171d392992ec67a26f712254d.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,34 @@
 import React from 'react'
-import Course from './components/Course'
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
 
 const App = () => {
   const course = {
-    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        exercises: 10
       },
       {
         name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+        exercises: 7
       },
       {
         name: 'State of a component',
-        exercises: 14,
-        id: 3
+        exercises: 14
       }
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
 }
 
 export default App
