3bb0cc2b3baa0b3f2b729cf0c53608e1b8a210ce
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-04-26 17:16:50 +0800

    part2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,8 +9,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({ name: newName }))
-    setNewName('')
+    if (persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+    }
   }
   
   const changeName = (event) => {
