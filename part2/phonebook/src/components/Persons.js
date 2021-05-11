1c27ea0733f0eb2b450fa3f2a12548d43932af9a
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-05-12 03:53:25 +0800

    delete-person (#30)

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Persons = ({persons}) => {
+const Persons = ({deletePerson, persons}) => {
   return (
     <div>
       {persons.map(person =>
-        <p key={person.id}>{person.name} {person.number}</p>
+        <p key={person.id}>{person.name} {person.number}{' '}
+          <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
+        </p>
       )}
     </div>
   )
