750d19623b1cc927422cee54cd3f341bea1fa56b
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-05-13 00:28:04 +0800

    update-person (#31)

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -7,13 +7,18 @@ const index = () => {
     .then(response => response.data)
 }
 
-const create = (newContact) => {
+const create = newContact => {
   return axios.post(baseUrl, newContact)
     .then(response => response.data)
 }
 
-const del = (id) => {
+const update = (id, newPerson) => {
+  return axios.put(`${baseUrl}${id}`, newPerson)
+    .then(response => response.data)
+}
+
+const del = id => {
   return axios.delete(`${baseUrl}${id}`)
 }
 
-export default { index, create, del }
+export default { index, create, update, del }
