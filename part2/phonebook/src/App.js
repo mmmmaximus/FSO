4015e593fe81ca87d10169ca3a3517697e844c89
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-05-13 23:24:53 +0800

    notification-message

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Input from './components/Input'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import ErrorMessage from './components/ErrorMessage'
 import phonebookService from './services/phonebook'
 
 const App = () => {
@@ -9,7 +10,16 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [errorMessage, setErrorMessage] = useState('')
+  const changeFilter = (event) => {
+    setNewFilter(event.target.value)
+  }
+  const changeNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const changeName = (event) => {
+    setNewName(event.target.value)
+  }
 
   useEffect(() => {
     phonebookService
@@ -22,7 +32,6 @@ const App = () => {
     if (newName === '' || newNumber === '') {
       alert(`Name and Number must be filled in`)
     } else if (persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
-
       if (persons.map(person => person.number).includes(newNumber)) {
         alert(`${newName} is already added to phonebook`)
       } else {
@@ -32,6 +41,10 @@ const App = () => {
             .update(changedPerson.id, { name: newName, number: newNumber })
             .then(person => {
               setPersons(persons.filter(person => person.name !== newName).concat(person))
+              setErrorMessage(`${person.name} has been updated`)
+              setTimeout(() => {
+                setErrorMessage('')
+              }, 1000)
             })
           setNewName('')
           setNewNumber('')
@@ -39,11 +52,16 @@ const App = () => {
           document.getElementById('number').value = ''
         }
       }
-
     } else {
       phonebookService
         .create({ name: newName, number: newNumber })
-        .then(person => setPersons(persons.concat(person)))
+        .then(person => {
+          setPersons(persons.concat(person))
+          setErrorMessage(`${person.name} has been added`)
+          setTimeout(() => {
+            setErrorMessage('')
+          }, 1000)
+        })
       setNewName('')
       setNewNumber('')
       document.getElementById('name').value = ''
@@ -57,24 +75,21 @@ const App = () => {
         .del(id)
         .then(phonebookService
           .index()
-          .then(persons => setPersons(persons.filter(person => person.id !== id)))
-      )
+          .then(persons => {
+            setPersons(persons.filter(person => person.id !== id))
+            setErrorMessage(`${name} has been deleted`)
+            setTimeout(() => {
+              setErrorMessage('')
+            }, 1000)
+          })
+        )
     }
-  }
-
-  const changeFilter = (event) => {
-    setNewFilter(event.target.value)
-  }
-  const changeNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
-  const changeName = (event) => {
-    setNewName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <ErrorMessage message={errorMessage}/>
       <Input text='filter shown with ' func={changeFilter} />
       <h2>add a new</h2>
       <PersonForm changeName={changeName} changeNumber={changeNumber} addContact={addContact}/>
