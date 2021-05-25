bb75f359ab38976b409b5e59655f78c4c61a53c8
Max Woon <thesugarhoneyicetea@hotmail.com>
2021-05-26 01:32:21 +0800

    app-test

--- part1/unicafe/src/components/Button.js
+++ part1/unicafe/src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+const Button = ({text, onClick}) => <button onClick={onClick} data-testid='Button'>{text}</button>
 
 export default Button
